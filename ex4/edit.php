6a9c7b7649f4e032e943af314085ab1ada2353e3
RubenV <rubenventura74@gmail.com>
2023-05-08 15:00:11 +0300

    ex4

---
+++ ex4/edit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>shoes</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <h3>Brand Name: <?php echo $_GET["name"]; ?></h3>
+    <h3>Model: <?php echo $_GET["model"]; ?></h3>
+    <h3>Size: <?php echo $_GET["size"]; ?></h3>
+</body>
+</html>
